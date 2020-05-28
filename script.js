0bd671f8feb0a403f3f2fbed7d18426a7456dbe2
Jadeosull93 <jadeosullivan@hotmail.com>
2020-05-28 12:56:14 +0000

    JS timer added and HTML/CSS scoreboard added to level1

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function unflipCards() {
     secondCard.classList.remove('flip');
 
     resetBoard();
-  }, 1500);
+  }, 1200);
 }
 
 function resetBoard() {
@@ -61,3 +61,62 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+//timer 
+//let stopTimer = false;//need this for the timer and the reset 
+let resetGame = true; //need this for game reset and modal 
+
+let hour = 0;
+let minute = 0;
+let second = 0;
+
+function restartGame() {
+    if (timeStart) stopTimer();
+    resetTimer();
+    cards.forEach(card => {
+        card.classList.remove('flip');
+        card.addEventListener('click', flipCard);
+    });
+    shuffle();
+    matchCount = 0;
+    moves = 0;
+    const movesText = document.querySelector('.moves');
+    movesText.innerHTML = moves;
+    aroo.play();
+}
+//restartBtn = document.getElementsByClassName("resetBtn");
+
+// restartBtn.addEventListener('click', function(e) {
+// });
+//     restartBtn = document.getElementsByClassName("resetBtn");
+function startTimer() {
+    if (resetGame == true) {
+        let timer = 0;
+        if (timeStart === "") {
+            timeStart = setInterval(() => { //8-30 if i use => timer works, change it per https://www.w3schools.com/js/js_timing.asp it acts like it wants to start but doesnt
+                ++timer;
+                second = timer % 60;
+                minute = Math.floor(timer / 60);
+                if (minute < 10) minute = '0' + minute;
+                if (second < 10) second = '0' + second;
+                //hour = Math.floor(timer / 3600);
+                //minute = Math.floor((timer - hour * 3600) / 60);
+                //second = timer - hour * 3600 - minute * 60;
+                //if (hour < 10) hour = '0' + hour;
+                //if (minute < 10) minute = '0' + second;
+                document.querySelector(".timer").innerHTML = minute + ':' + second;
+                document.querySelector(".clock").innerHTML = minute + ':' + second;
+            }, 1000);
+        }
+    }
+}
+
+function resetTimer() {
+    document.querySelector(".timer").innerHTML = '00:00';
+    [hour, minute, second] = [0, 0, 0];
+}
+
+function stopTimer() {
+    clearInterval(timeStart); //clearInterval needs to use the variable from the setInterval 
+    timeStart = '';
+}

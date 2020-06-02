bcd53f7e831c8bbaa39d2f96f43b0a4dafd01ee8
Jadeosull93 <jadeosullivan@hotmail.com>
2020-06-02 20:55:38 +0000

    JS added to dedug while writing more js code

--- script.js
+++ script.js
@@ -1,24 +1,74 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let debug=true;
+let shuffleOn=true;
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
 let timeStart = "";
 let matchCount= 0;
 let totalGameMovesElement = document.getElementById('totalGameMoves');
+let moves = document.getElementsByClassName('.moves');
 let totalGameTimeElement = document.getElementById('totalGameTime');
 let finalStarRatingElement = document.getElementById('finalStarRating');
-let closeModalIcon = document.getElementById('closeModal');
+let closeModalBtn = document.getElementById('closeModal');
 let modal = document.getElementById('levelCompleteModal');
+let movesCount = 0;
+let closeBtn = document.getElementsByClassName('closeBtn')[0];
+//if ((closeBtn) && (typeof closeModal === "function") ) 
+//	closeBtn.addEventListener('click', closeModal());
+//else
+//	console.log("Could not add listener for element, it does not exist");
+assignClickListner(closeModalBtn,closeModal);
 
 
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
+//timer 
+//let stopTimer = false;//need this for the timer and the reset 
+let resetGame = true; //need this for game reset and modal 
+
+let second = 0,
+    minute = 0,
+    hour = 0,
+    interval,
+    totalGameTime,
+    starRating;
+
+//Add Listners
+
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+let resetBtn = document.getElementById('resetBtn')
+resetBtn.addEventListener('click', resetPlay, false)
+
+
+
+startNewGame()
+
+function assignClickListner(element,func)
+{
+	if ((element) && (typeof func === "function") ) 
+		element.addEventListener('click', func);
+	else
+	{
+		if(element)
+			console.log("Could not add listner, function does not exist");
+		else
+			console.log("Could not add listener for element " + element + " it does not exist");
+	}
+}
+
+function flipCard()
+{
+  if (lockBoard)
+    return;
+  if (this === firstCard)
+    return;
 
   this.classList.add('flip');
     console.log("Flippen heck");
-  if (!hasFlippedCard) {
+  if (!hasFlippedCard)
+  {
     // first click
     hasFlippedCard = true;
     firstCard = this;
@@ -45,11 +95,11 @@ function checkForMatch() {
      if(matchCount >= 6)
      {
         gameOver();
-        console.log("game over")
+        log("game over")
      }
      else
      {
-        console.log("Game continues... matchCount is:" + matchCount);
+        log("Game continues... matchCount is:" + matchCount);
      }
  }
  else
@@ -57,8 +107,8 @@ function checkForMatch() {
       unflipCards();
  }
 
- 
-  console.log("IS MATCH " + isMatch);
+ 	log("Matched: " + isMatch);
+
 }
 
 function disableCards() {
@@ -85,156 +135,14 @@ function resetBoard() {
 }
 
 function shuffle() {
+if(shuffleOn)
+{			
     cards.forEach(card => {
         let randomPos = Math.floor(Math.random() * 12);
+	log("random = " + randomPos);
         card.style.order = randomPos;
 
     });
 }
-
-let moves = 0;
-
-function addMove() {
-    moves++;
-    const movesText = document.querySelector('.moves');
-    movesText.innerHTML = moves;
-
-}
-if (hasFlippedCard.length === 2) {
-    //checkForMatch(clickTarget);
-
-}
-//timer 
-//let stopTimer = false;//need this for the timer and the reset 
-let resetGame = true; //need this for game reset and modal 
-
-let second = 0,
-    minute = 0,
-    hour = 0,
-    interval,
-    totalGameTime,
-    starRating;
-
-function startTimer() {
-    interval = setInterval(function(){
-        let currentTime = `${minute} mins ${second} secs`;
-        if(currentTime != null)
-            timer.innerHTML = currentTime;
-        second++;
-        if(second == 60) {
-            minute++;
-            second = 0;
-        }
-        if(minute == 60) {
-            hour++;
-            minute = 0;
-        }
-    }, 1000)
-}    
-
-
-
-
-function gameOver() {
-    //totalGameTime = timer.innerHTML;
-    
-
-    //show modal on game end
-    openModal();
-    
-    //show totalGameTime, moves
-   // if( totalGameTime != null)
-        //totalGameTimeElement.innerHTML = totalGameTime;
-    //if(moves != null)
-        totalGameMovesElement.innerHTML = moves;
-   
-
-  //  matchedCards = [];
-    closeModal();
 }
 
-let closeBtn = document.getElementsByClassName('closeBtn')[0];
-
-//Listen for close click
-closeBtn.addEventListener('click', closeModal);
-
-function closeModal() {
-    modal.style.display = 'none'; //element will not be displayed 
-}
-
-
-function openModal() {
-    modal.style.display = 'block'; // this is rendered as a block level element 
-}
-
-function closeModal() {
-    closeModalIcon.addEventListener("click", function() {
-        modalElement.classList.remove("show-modal");
-        startGame();
-    })
-}
-
-//restartBtn = document.getElementsByClassName("resetBtn");
-
-// restartBtn.addEventListener('click', function(e) {
-// });
-//     restartBtn = document.getElementsByClassName("resetBtn");
-function startTimer() {
-    if (resetGame == true) {
-        let timer = 0;
-        if (timeStart === "") {
-            timeStart = setInterval(() => { //8-30 if i use => timer works, change it per https://www.w3schools.com/js/js_timing.asp it acts like it wants to start but doesnt
-                ++timer;
-                second = timer % 60;
-                minute = Math.floor(timer / 60);
-                if (minute < 10) minute = '0' + minute;
-                if (second < 10) second = '0' + second;
-                //hour = Math.floor(timer / 3600);
-                //minute = Math.floor((timer - hour * 3600) / 60);
-                //second = timer - hour * 3600 - minute * 60;
-                //if (hour < 10) hour = '0' + hour;
-                //if (minute < 10) minute = '0' + second;
-                let elaspedTime = minute + ':' + second;
-                if(elaspedTime != null)
-                {
-                       // console.log("elapsedTime " + elaspedTime);
-                        document.querySelector(".timer").innerHTML = elaspedTime;
-                        //document.querySelector(".clock").innerHTML = elaspedTime;
-                }
-
-            }, 1000);
-        }
-    }
-}
-
-function resetTimer() {
-    document.querySelector(".timer").innerHTML = '00:00';
-    [hour, minute, second] = [0, 0, 0];
-}
-
-function stopTimer() {
-    clearInterval(timeStart); //clearInterval needs to use the variable from the setInterval 
-    timeStart = '';
-}
-
-let resetBtn =
-    document.getElementById('resetBtn')
-resetBtn.addEventListener('click', resetPlay, false)
-
-
-function resetPlay() {
-    stopTimer();
-    resetTimer();
-    resetMoves();
-    
-
-}
-
-function resetMoves() {
-    const movesText = document.querySelector('moves');
-    let moves = 0;
-    movesText.innerHTML = moves;
-
-
-}
-cards.forEach(card => card.addEventListener('click', flipCard));

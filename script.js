612e99c56209ddfedd0ee113afde4e2239e6e9ce
Jadeosull93 <jadeosullivan@hotmail.com>
2020-06-03 20:06:19 +0000

    debugging issues with shuffle in JS

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ let closeBtn = document.getElementsByClassName('closeBtn')[0];
 //else
 //	console.log("Could not add listener for element, it does not exist");
 assignClickListner(closeModalBtn,closeModal);
-
+assignClickListner(resetBtn,resetPlay);
 
 //timer 
 //let stopTimer = false;//need this for the timer and the reset 
@@ -35,15 +35,17 @@ let second = 0,
 
 //Add Listners
 
-
-cards.forEach(card => card.addEventListener('click', flipCard));
-
-let resetBtn = document.getElementById('resetBtn')
-resetBtn.addEventListener('click', resetPlay, false)
-
+if(cards)
+	cards.forEach(card => card.addEventListener('click', flipCard));
+else
+	log("Cards not defined");
 
 
-startNewGame()
+
+
+
+
+initGame()
 
 function assignClickListner(element,func)
 {
@@ -56,6 +58,27 @@ function assignClickListner(element,func)
 		else
 			console.log("Could not add listener for element " + element + " it does not exist");
 	}
+}
+
+function assignPictures()
+{
+	var img = document.getElementsByTagName("img");
+	var imgSrcs = [];
+	let images = [];
+
+	for (var i = 0; i < img.length; i++) {
+		imgSrcs.push(img[i].src);
+		log(img[i].src);
+		//img[i].src = "http://192.168.1.15/img/pug.png"
+	}
+
+	for (var i = 1; i < 13; i++) {
+		images.push(document.getElementById("img" + i));
+		log("Image id = " + images[i-1].src);
+	}
+	
+	//return imgSrcs;
+
 }
 
 function flipCard()
@@ -134,15 +157,153 @@ function resetBoard() {
   [firstCard, secondCard] = [null, null];
 }
 
-function shuffle() {
-if(shuffleOn)
-{			
-    cards.forEach(card => {
-        let randomPos = Math.floor(Math.random() * 12);
-	log("random = " + randomPos);
-        card.style.order = randomPos;
+(function shuffle() {
+  assignPictures();
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+    log("Assigning values to cards: " + randomPos);	
+  });
+})();
 
-    });
+
+
+function addMove() {
+    movesCount++;
+    moves.innerHTML = movesCount;
+
 }
+
+
+function startTimer() {
+    interval = setInterval(function(){
+        let currentTime = `${minute} mins ${second} secs`;
+        if(currentTime != null)
+            timer.innerHTML = currentTime;
+        second++;
+        if(second == 60) {
+            minute++;
+            second = 0;
+        }
+        if(minute == 60) {
+            hour++;
+            minute = 0;
+        }
+    }, 1000)
+}    
+
+
+function log(textToLog)
+{
+	if(debug)
+		console.log(textToLog);
 }
+
+function initGame()
+{
+	
+	hasFlippedCard = false;
+	lockBoard = false;
+	timeStart = "";
+	matchCount= 0;
+	moves=0;
+	resetPlay();
+	log("Staring new Game");
+}
+
+function gameOver() {
+    //totalGameTime = timer.innerHTML;
+    
+
+    //show modal on game end
+    openModal();
+    
+    //show totalGameTime, moves
+   // if( totalGameTime != null)
+        //totalGameTimeElement.innerHTML = totalGameTime;
+    //if(moves != null)
+        totalGameMovesElement.innerHTML = movesCount;
+   
+
+  //  matchedCards = [];
+    //closeModal();
+}
+
+
+function closeModal() {
+	log("Closing Model");
+    	modal.style.display = 'none'; //element will not be displayed 
+	initGame();
+}
+
+
+function openModal() {
+    modal.style.display = 'block'; // this is rendered as a block level element 
+}
+
+
+
+//restartBtn = document.getElementsByClassName("resetBtn");
+
+// restartBtn.addEventListener('click', function(e) {
+// });
+//     restartBtn = document.getElementsByClassName("resetBtn");
+function startTimer() {
+    if (resetGame == true) {
+        let timer = 0;
+        if (timeStart === "") {
+            timeStart = setInterval(() => { //8-30 if i use => timer works, change it per https://www.w3schools.com/js/js_timing.asp it acts like it wants to start but doesnt
+                ++timer;
+                second = timer % 60;
+                minute = Math.floor(timer / 60);
+                if (minute < 10) minute = '0' + minute;
+                if (second < 10) second = '0' + second;
+                //hour = Math.floor(timer / 3600);
+                //minute = Math.floor((timer - hour * 3600) / 60);
+                //second = timer - hour * 3600 - minute * 60;
+                //if (hour < 10) hour = '0' + hour;
+                //if (minute < 10) minute = '0' + second;
+                let elaspedTime = minute + ':' + second;
+                if(elaspedTime != null)
+                {
+                       // console.log("elapsedTime " + elaspedTime);
+                        document.querySelector(".timer").innerHTML = elaspedTime;
+                        //document.querySelector(".clock").innerHTML = elaspedTime;
+                }
+
+            }, 1000);
+        }
+    }
+}
+
+function resetTimer() {
+    document.querySelector(".timer").innerHTML = '00:00';
+    [hour, minute, second] = [0, 0, 0];
+}
+
+function stopTimer() {
+    clearInterval(timeStart); //clearInterval needs to use the variable from the setInterval 
+    timeStart = '';
+}
+
+
+
+
+function resetPlay() {
+    stopTimer();
+    resetTimer();
+    resetMoves();
+    
+
+}
+
+function resetMoves() {
+    
+    movesCount = 0;
+    moves.innerHTML = movesCount;
+
+
+}
+
+
 
